c325cef8da4c9f6c601a6882237052c3a7024555
pgg1309 <pgrahl@gmail.com>
2023-08-07 16:38:01 -0300

    added function do download NIPA table

---
+++ R/get_bea_table.R
@@ -0,0 +1,46 @@
+#' Download NIPA table from BEA website
+#'
+#' @param TableName A string with table name (see https://apps.bea.gov/api/_pdf/bea_web_service_api_user_guide.pdf)
+#' @param Frequency A string with frequency (e.g. 'A,Q') - no spaces
+#' @param Year A string with year (e.g. '2002,2003'); no spaces - default 'ALL'
+#' @param UserID A string with BEA API id
+#'
+#' @return A JSON list with data and metadata
+#' @export
+#'
+#' @examples
+#' \dontrun{my_id <- options()$BEA_API
+#' #' get_bea_nipa("T10106", "Q", "2022,2023", my_id)}
+get_bea_nipa <- function(TableName = NULL,
+                         Frequency = 'Q',
+                         Year = 'ALL',
+                         UserID = NULL) {
+  if (is.null(TableName))
+    stop("Table Name cannot be NULL")
+  if (is.null(UserID))
+    stop("User ID is needed (BEA API)")
+
+
+  URI <- 'https://apps.bea.gov/api/data'
+
+  # NIPA
+  # required: TableName, Frequency, Year (X=ALL)
+  #
+
+  # BLS API request
+  req <- httr2::request(URI) |>
+    httr2::req_url_path_append(
+      glue::glue(
+        '?&UserID={UserID}&method=GetData&datasetname=NIPA&TableName={TableName}&Frequency={Frequency}&Year={Year}&ResultFormat=JSON'
+      )
+    ) |>
+    httr2::req_retry(max_tries = 10)
+
+  resp <- req |> httr2::req_perform()
+
+  json_data <- resp |> httr2::resp_body_string() |>
+    jsonlite::fromJSON()
+
+  return(json_data)
+}
+
